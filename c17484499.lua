895f3ef181672447907303af312780db0ed2ec9f
mercury233 <me@mercury233.me>
2016-05-18 18:55:26 +0800

    restore original errata cards

--- c17484499.lua
+++ c17484499.lua
@@ -1,11 +1,10 @@
 --現世と冥界の逆転
 function c17484499.initial_effect(c)
-	--Activate
+	--Activate(summon)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(0,TIMING_DRAW_PHASE)
-	e1:SetCountLimit(1,17484499+EFFECT_COUNT_CODE_DUEL)
 	e1:SetCondition(c17484499.condition)
 	e1:SetCost(c17484499.cost)
 	e1:SetOperation(c17484499.activate)
@@ -13,11 +12,10 @@ function c17484499.initial_effect(c)
 end
 function c17484499.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetFieldGroupCount(tp,LOCATION_GRAVE,0)>=15
-		and Duel.GetFieldGroupCount(tp,0,LOCATION_GRAVE)>=15
 end
 function c17484499.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.CheckLPCost(tp,1000) end
-	Duel.PayLPCost(tp,1000)
+	if chk==0 then return Duel.CheckLPCost(tp,1000)
+	else Duel.PayLPCost(tp,1000) end
 end
 function c17484499.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.SwapDeckAndGrave(tp)
