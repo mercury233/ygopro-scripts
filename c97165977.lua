fcbf0b25684cb532b97710c8c29615b1de3bb307
mercury233 <me@mercury233.me>
2016-06-12 18:14:33 +0800

    temp fix for Lunalight Dancers
    https://github.com/Fluorohydride/ygopro/issues/1772

--- c97165977.lua
+++ c97165977.lua
@@ -56,18 +56,48 @@ function c97165977.operation(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
 	if c:IsRelateToEffect(e) then
+		--infinte atk to monster
 		local e2=Effect.CreateEffect(c)
 		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_ATTACK_ALL)
+		e2:SetCode(EFFECT_EXTRA_ATTACK)
 		e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
-		e2:SetValue(2)
+		e2:SetValue(9999)
+		e2:SetCondition(c97165977.atkcon)
 		c:RegisterEffect(e2)
+		--reg attacked flag to battle target
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+		e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+		e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e3:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		e3:SetOperation(c97165977.tmpatkop)
+		c:RegisterEffect(e3)
+		--can not attack one monster more than 2 times
+		local e4=Effect.CreateEffect(c)
+		e4:SetType(EFFECT_TYPE_SINGLE)
+		e4:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+		e4:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		e4:SetValue(c97165977.atktg)
+		c:RegisterEffect(e4)
 	end
 end
 function c97165977.indct(e,re,r,rp)
 	if bit.band(r,REASON_BATTLE)~=0 then
 		return 1
 	else return 0 end
+end
+function c97165977.atkcon(e)
+	return Duel.GetFieldGroupCount(e:GetHandlerPlayer(),0,LOCATION_MZONE)>0
+end
+function c97165977.tmpatkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=c:GetBattleTarget()
+	if tc then
+		tc:RegisterFlagEffect(97165977,RESET_PHASE+PHASE_END,0,1)
+	end
+end
+function c97165977.atktg(e,c)
+	return c:GetFlagEffect(97165977)>=2
 end
 function c97165977.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
