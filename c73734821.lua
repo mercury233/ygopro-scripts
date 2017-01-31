6aa817b7333ef1ca72ff29f64501ff4d4df32e96
mercury233 <me@mercury233.me>
2017-02-01 01:32:47 +0800

    temp fix for Performapal Sky Magician
    https://github.com/Fluorohydride/ygopro/issues/1924

--- c73734821.lua
+++ c73734821.lua
@@ -68,7 +68,9 @@ function c73734821.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 end
 function c73734821.tffilter(c,tp)
-	return c:IsSetCard(0x98) and c:GetType()==TYPE_SPELL+TYPE_CONTINUOUS and c:GetActivateEffect():IsActivatable(tp)
+	return c:IsSetCard(0x98) and c:GetType()==TYPE_SPELL+TYPE_CONTINUOUS
+		and c:GetActivateEffect()
+		and (c:GetActivateEffect():IsActivatable(tp) or Duel.GetTurnPlayer()~=tp)
 end
 function c73734821.thop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
